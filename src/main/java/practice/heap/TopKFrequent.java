940923d339c8fa04b06e958eca8d9f7155e4efaa
Jezzis <jezzis727@126.com>
2020-12-31 00:54:08 +0800

    feat: update

--- src/main/java/practice/heap/TopKFrequent.java
+++ src/main/java/practice/heap/TopKFrequent.java
@@ -61,4 +61,63 @@ public class TopKFrequent {
         }
         return ret;
     }
+
+    public int[] topKFrequent(int[] nums, int k) {
+        Map<Integer, Integer> counter = new HashMap<>();
+        for (int n : nums)
+            counter.put(n, counter.getOrDefault(n, 0) + 1);
+
+        int c = 0;
+        int[][] arr = new int[counter.size()][2];
+        for (int key : counter.keySet())
+            arr[c++] = new int[]{key, counter.get(key)};
+        int[] res = new int[k];
+        if (counter.size() == 1) {
+            res[0] = nums[0];
+            return res;
+        }
+        qsort(arr, 0, arr.length - 1, k, res);
+        print(arr);
+        for (int n = 0; n < k; n++)
+            res[n] = arr[n][0];
+        return res;
+    }
+
+    public void print(int[][] arr) {
+        System.out.printf("[\n");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.printf(" [");
+            for (int j = 0; j < arr[i].length; j++)
+                System.out.printf("%d, ", arr[i][j]);
+            System.out.printf("]\n");
+        }
+        System.out.printf("]\n");
+    }
+
+    public void qsort(int[][] arr, int start, int end, int k, int[] res) {
+        System.out.printf("-> start=%d, end=%d, k=%d\n", start, end, k);
+        print(arr);
+        if (start >= end) return;
+        int pivot = arr[start][1];
+        int i = start, j = end;
+        while (i < j) {
+            while (i < j && pivot >= arr[j][1]) j--;
+            if (i < j) swap(arr, i++, j);
+            while (i < j && pivot < arr[i][1]) i++;
+            if (i < j) swap(arr, i, j--);
+        }
+        if (k <= i - start) {
+            qsort(arr, start, i - 1, k, res);
+        } else if (k > i - start + 1) {
+            qsort(arr, i + 1, end, k - (i - start + 1), res);
+        }
+    }
+
+    public void swap(int[][] arr, int i, int j) {
+        for (int k = 0; k < arr[i].length; k++) {
+            int temp = arr[i][k];
+            arr[i][k] = arr[j][k];
+            arr[j][k] = temp;
+        }
+    }
 }
